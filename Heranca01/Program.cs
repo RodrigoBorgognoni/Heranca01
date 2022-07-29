ea48f55db124400a1f5d3fd2f54b9f4acdef27ea
RodrigoBorgognoni <rodrigoborgognoni@gmail.com>
2022-07-28 21:28:00 -0300

    Fim

---
+++ Heranca01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Heranca01.Entities;
+
+namespace Heranca01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            // parâmetro Balance está como protected, ou seja, só pode ser modificado diretamente pela Classe e pela subclasse
+            //account.Balance = 1000;
+        }
+    }
+}
