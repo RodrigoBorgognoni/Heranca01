3f88b29d32edbc25ff4d5da30ed244e859e3faa2
RodrigoBorgognoni <rodrigoborgognoni@gmail.com>
2022-08-02 19:44:32 -0300

    adicionado taxa para saques em Account

--- Heranca01/Entities/SavingsAccount.cs
+++ Heranca01/Entities/SavingsAccount.cs
@@ -18,5 +18,21 @@
         {
             Balance += Balance * InterestRate;
         }
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        /* 
+        Utilizando base. podemos reutilizar o método da Superclasse em nossa subclasse
+        e aplicar outras regras, no caso, além dos 5 reais de taxa para saque, a conta corrente terá
+        2 reais de taxa adicionais 
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+        */
     }
 }
--- Heranca01/Program.cs
+++ Heranca01/Program.cs
@@ -7,34 +7,15 @@ namespace Heranca01
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Account acc1 = new Account(1001,"Alex",500.0);
+            Account acc2 = new SavingsAccount(1002,"Ana",500.0, 0.01);
 
-            Account acc = new Account(1001, "Alex", 0.0);
-            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+            acc1.Withdraw(10);
+            acc2.Withdraw(10);
 
-            //upcasting
-
-            Account acc1 = bacc;
-            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
-
-            //Downcasting
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-            acc4.Loan(100.0);
-
-            //BusinessAccount acc5 = (BusinessAccount)acc3;
-            if (acc3 is BusinessAccount)
-            {
-                BusinessAccount acc5 = (BusinessAccount)acc3;
-                acc5.Loan(200.0);
-                Console.WriteLine("Loan!");
-            }
-            if (acc3 is SavingsAccount)
-            {
-                SavingsAccount acc5 = (SavingsAccount)acc3;
-                acc5.UpdateBalance();
-                Console.WriteLine("Update");
-            }
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
         }
+
     }
 }
