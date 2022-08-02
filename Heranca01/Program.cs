e280e74507ab550d142a9d1a059df88535d8721e
RodrigoBorgognoni <rodrigoborgognoni@gmail.com>
2022-08-02 13:47:37 -0300

    Adicionado classe savings account

--- Heranca01/Program.cs
+++ Heranca01/Program.cs
@@ -9,10 +9,32 @@ namespace Heranca01
         {
             BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
-            // parâmetro Balance está como protected, ou seja, só pode ser modificado diretamente pela Classe e pela subclasse
-            //account.Balance = 1000;
+            //upcasting
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            //Downcasting
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
         }
     }
 }
